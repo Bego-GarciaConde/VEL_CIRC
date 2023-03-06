b0c2f7ea338e14cc62ed9eb883495a81b51fb5c4
Bego García-Conde <91980267+Bego-GarciaConde@users.noreply.github.com>
2023-03-06 13:08:34 +0100

    added comments

--- mesh_utils.py
+++ mesh_utils.py
@@ -64,7 +64,10 @@ class Mesh:
     
 
         def snapshot_to_grid (nbins = 200):
-
+            """Initializes a mesh in which we are going to calculate accelerations
+            This mesh is situated in the plane Z=0, which should correspond with the
+            galactic plane if well aligned
+            """
             mesh_x = np.zeros(nbins**2, dtype = np.float32)
             mesh_y =  np.zeros(nbins**2, dtype = np.float32)
             mesh_r = np.zeros(nbins**2, dtype = np.float32)
@@ -72,6 +75,7 @@ class Mesh:
             x_array = y_array = np.linspace(-limit, +limit, nbins)
 
             m = 0
+            #Square mesh, so it is more "uniform" comparing inner and outer radii
             for x in x_array:
                 for y in y_array:
                     mesh_x[m],mesh_y[m] = x,y
@@ -79,7 +83,7 @@ class Mesh:
                     mesh_r[m] = np.sqrt(x**2 +y**2)
                     m= m+1
 
-            res = [idx for idx, val in enumerate(mesh_r) if val < limit]
+            res = [idx for idx, val in enumerate(mesh_r) if val < limit]  #Radial limit for mesh
             self.x = mesh_x[res]
             self.y = mesh_y[res]
             self.z = mesh_z[res]
@@ -90,27 +94,9 @@ class Mesh:
 
 
     def calculate_force(self, x_comp, y_comp, z_comp, mass_comp):
-       # limite_prueba = 20
-        #counter = 0
-
-        # ax_tot = []
-        # ay_tot = []
-        # for (i, j, k) in zip(self.x, self.y, self.z):
-        #     coord = [i,j,k]
-        #     r_sph = np.sqrt(i**2 +j**2 + k**2)
-        #     ind = np.where(np.sqrt(x_comp**2+y_comp**2 + z_comp**2)<r_sph)
-        #     ax, ay = force_in_mesh_gen (coord, x_comp[ind[0]], y_comp[ind[0]], z_comp[ind[0]], mass_comp[ind[0]])
-        #    # print(f"for bins {i,j,k} acc {ax, ay}")
-        #     progressbar(counter, len(self.x))
-        #     ax_tot.append(ax)
-        #     ay_tot.append(ay)
-        #     counter=counter +1
-         #   print(f"counter {counter} and length {len(ax_tot) }")
-      #      if counter > limite_prueba:
-       #         break
-
-      #  return ax_tot, ay_tot
-        pool = Pool(6)
+        """Divides in threads and calculates forces/accelerations
+        """
+        pool = Pool(6) #Multiprocessing
 
         coord = []
         for i,j,k in zip(self.x, self.y, self.z):
@@ -126,10 +112,7 @@ class Mesh:
 
         ax_tot =  [item[0] for item  in res ] 
         ay_tot =  [item[1] for item  in res ] 
-        
-      #  data ={'X':self.x, 'Y':self.y,  'Z':self.z, 'R':self.R,'Phi':self.phi, 'az': az_halo}
 
-      #  mesh_completa = pd.DataFrame(data)
         return ax_tot, ay_tot
 
 
@@ -139,38 +122,38 @@ class Mesh:
         snapshot.load_stars()
         snapshot.load_dm()
         snapshot.load_gas()
-      #  ax_halo = np.zeros(len(self.x), dtype = np.float64)
-      #  ay_halo = np.zeros(len(self.y), dtype = np.float64)
-     #   az_halo = np.zeros(len(self.z), dtype = np.float64)
 
-        x_dm, y_dm, z_dm, mass_dm = extract_coord_mass(snapshot.dm)
-       # dm_ax, dm_ay = self.calculate_force(x_dm, y_dm, z_dm, mass_dm)    
+        x_dm, y_dm, z_dm, mass_dm = extract_coord_mass(snapshot.dm) 
         print("Dark matter completed!")
+
         x_gas, y_gas, z_gas, mass_gas = extract_coord_mass(snapshot.gas)
-       # gas_ax, gas_ay = self.calculate_force(x_gas, y_gas, z_gas, mass_gas)    
         print("Gas completed!")
+        
         x_stars, y_stars, z_stars, mass_stars = extract_coord_mass(snapshot.stars)
-       # stars_ax, stars_ay = self.calculate_force(x_stars, y_stars, z_stars, mass_stars)    
         print("Stars completed!")
-      #  x_tot = np.concatenate((x_dm, x_gas, x_stars), axis=0)
-      #  y_tot = np.concatenate((y_dm, y_gas, y_stars), axis=0)
-      #  z_tot = np.concatenate((z_dm, z_gas, z_stars), axis=0)
+
         mass_tot = np.concatenate((mass_dm, mass_gas, mass_stars), axis=0)
 
+        #Calculates forces/accelerations in X-Y plane
         dm_ax, dm_ay = self.calculate_force(x_dm, y_dm, z_dm, mass_tot)  
         stars_ax, stars_ay = self.calculate_force(x_stars, y_stars, z_stars, mass_stars)  
         gas_ax, gas_ay = self.calculate_force(x_gas, y_gas, z_gas, mass_gas)  
+
+        #Fom X,Y to R
         ar_dm = (self.x*dm_ax + self.y*dm_ay)/self.R
         ar_gas = (self.x*gas_ax + self.y*gas_ay)/self.R
         ar_stars = (self.x*stars_ax + self.y*stars_ay)/self.R
-      #  total_ax, total_ay = self.calculate_force(x_tot, y_tot, z_tot, mass_tot)    
-       # ar_total = (self.x*total_ax + self.y*total_ay)/self.R
+
         ar_total = ar_dm + ar_gas + ar_stars
         self.Vcirc = np.sqrt(ar_total*self.R*kpc_to_km)
-        data ={'X':self.x, 'Y':self.y,  'Z':self.z, 'R':self.R,'Phi':self.phi,'ar_dm':ar_dm, 'a_stars':ar_stars, 'ar_gas':ar_gas, 'ar':ar_total, 'Vcirc':np.sqrt(-ar_total*self.R*kpc_to_km)}
+
+        #Save total ar and for every component, in case we want to compare
+        data ={'X':self.x, 'Y':self.y,  'Z':self.z, 'R':self.R,'Phi':self.phi,'ar_dm':ar_dm, 'a_stars':ar_stars, 
+        'ar_gas':ar_gas, 'ar':ar_total, 'Vcirc':np.sqrt(-ar_total*self.R*kpc_to_km)}
     
         mesh_completa = pd.DataFrame(data)
         print(mesh_completa["Vcirc"])
+
         mesh_completa.to_csv(f"results/Vcirc_mesh_{self.name}_nogauss_components.csv", sep = ",")
             
     def plot_Vcirc (self):
